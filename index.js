068ab403a64242f142ee56faf5bcb3cfcc44b53f
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-06-06 00:49:30 -0600

    functions lab converting blocks into feet by 1 block = 264 feet, but has one bad practice returning two different (relatively speaking) types in the same function

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(loc)
+{
+    if (loc < 42) return 42 - loc;
+    else if (loc > 42) return loc - 42;
+    else return 0;
+}
+
+function distanceFromHqInFeet(loc)
+{
+    return distanceFromHqInBlocks(loc) * 264;
+}
+
+function distanceTravelledInFeet(start, dest)
+{
+    var diff = 0;
+    if (start > dest) diff = start - dest;
+    else if (dest > start) diff = dest - start;
+    else return 0;
+    return diff * 264;
+}
+
+function calculatesFarePrice(start, dest)
+{
+    //console.log("start = " + start);
+    //console.log("dest = " + dest);
+    
+    var diff = distanceTravelledInFeet(start, dest);
+    
+    //console.log("diff = " + diff);
+
+    if (diff < 400 || diff == 400) return 0;
+    else if (diff > 400 && diff < 2000)
+    {
+        return (diff - 400) * 2 / 100;
+    }
+    else if ((diff > 2000 || diff == 2000) && diff < 2500)
+    {
+        return 25;
+    }
+    else return "cannot travel that far";
+    //NOTE: EXTREMELY BAD PRACTICE
+    //IT IS BETTER TO THROW THE ERROR OR KEEP THE RETURN TYPE THE SAME
+    //AND HAVE AN ERROR VALUE ASSOCIATED WITH THE ERROR
+}
